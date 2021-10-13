45d943f9c329420e20642ffabffa703bd529fcdd
Maira Zholdasbaeva <mairakkyzy@gmail.com>
2021-10-13 12:23:21 +0600

    change in text

--- lib/main.dart
+++ lib/main.dart
@@ -70,7 +70,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Guess my birthday'),
+        title: Text('Hi Nursultan!'),
       ),
       body: Column(
         children: [
