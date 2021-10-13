e0de0af1f251f2eeaed04f0bf2a10e95271cd14b
Altynay Mukhtar <190107054@stu.sdu.edu.kz>
2021-10-14 04:33:15 +0600

    deleted home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,14 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'dart:math';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: _buildShrineTheme(),
+      title: 'Flutter Demo',
       home: MyHomePage(),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -22,72 +19,114 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int x = 0;
-  int degree = 0;
+  int _currentIndex = 0;
 
-  String getText() {
-    if (degree == 0) {
-      return "1     3   5    7\n9   11 13 15\n17 19 21 23\n25 27 29 31";
-    } else if (degree == 1) {
-      return "2     3    6   7\n10 11 14 15\n18 19 22 23\n26 27 30 31";
-    } else if (degree == 2) {
-      return "4     5    6   7\n12 13 14 15\n20 21 22 23\n28 29 30 31";
-    } else if (degree == 3) {
-      return "8     9  10 11\n12 13 14 15\n24 25 26 27\n28 29 30 31";
-    } else if (degree == 4) {
-      return "16 17 18 19\n20 21 22 23\n24 25 26 27\n28 29 30 31";
-    } else {
-      return "Your BD is $x";
-    }
-  }
-
-  void nextState(addNumber) {
-    setState(() {
-      if (addNumber) {
-        x += pow(2, degree);
-      }
-      degree += 1;
-    });
-  }
-
-  Widget getControls() {
-    if (degree < 5) {
-      return Row(
-        children: [
-          Spacer(),
-          ElevatedButton(onPressed: () => nextState(false), child: Text("No")),
-          SizedBox(width: 10),
-          ElevatedButton(onPressed: () => nextState(true), child: Text("Yes")),
-          Spacer()
-        ],
-      );
-    } else {
-      return Spacer();
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    final textTheme = Theme.of(context).textTheme;
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Hi Maira'),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: Text(
+                "Book",
+                textAlign: TextAlign.left,
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.w600),
+              ),
+            ),
+            Image.asset('assets/images/baner.png'),
+          ],
+        ),
       ),
-      body: Column(
-        children: [
-          SizedBox(height: 10),
-          Text(
-            "Do you see your BD here?",
-            style: TextStyle(fontSize: 24),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _currentIndex,
+        backgroundColor: colorScheme.surface,
+        selectedItemColor: colorScheme.onSurface,
+        unselectedItemColor: colorScheme.onSurface.withOpacity(.60),
+        selectedLabelStyle: textTheme.caption,
+        unselectedLabelStyle: textTheme.caption,
+        onTap: (value) {
+          // Respond to item press.
+          setState(() => _currentIndex = value);
+        },
+        items: [
+          BottomNavigationBarItem(
+            title: Text('Home'),
+            icon: Icon(Icons.home),
           ),
-          SizedBox(height: 10),
-          Text(
-            getText(),
-            style: TextStyle(fontSize: 18),
+          BottomNavigationBarItem(
+            title: Text('Search'),
+            icon: Icon(Icons.search),
           ),
-          SizedBox(height: 10),
-          getControls()
+          BottomNavigationBarItem(
+            title: Text('Settings'),
+            icon: Icon(Icons.settings),
+          ),
         ],
       ),
     );
   }
 }
+
+ThemeData _buildShrineTheme() {
+  final ThemeData base = ThemeData.light();
+  return base.copyWith(
+    colorScheme: _shrineColorScheme,
+    textTheme: _buildShrineTextTheme(base.textTheme),
+  );
+}
+
+TextTheme _buildShrineTextTheme(TextTheme base) {
+  return base
+      .copyWith(
+        caption: base.caption.copyWith(
+          fontWeight: FontWeight.w400,
+          fontSize: 14,
+          letterSpacing: defaultLetterSpacing,
+        ),
+        button: base.button.copyWith(
+          fontWeight: FontWeight.w500,
+          fontSize: 14,
+          letterSpacing: defaultLetterSpacing,
+        ),
+      )
+      .apply(
+        fontFamily: 'Rubik',
+        displayColor: shrineBrown900,
+        bodyColor: shrineBrown900,
+      );
+}
+
+const ColorScheme _shrineColorScheme = ColorScheme(
+  primary: shrinePink100,
+  primaryVariant: shrineBrown900,
+  secondary: shrinePink50,
+  secondaryVariant: shrineBrown900,
+  surface: shrineSurfaceWhite,
+  background: shrineBackgroundWhite,
+  error: shrineErrorRed,
+  onPrimary: shrineBrown900,
+  onSecondary: shrineBrown900,
+  onSurface: shrineBrown900,
+  onBackground: shrineBrown900,
+  onError: shrineSurfaceWhite,
+  brightness: Brightness.light,
+);
+
+const Color shrinePink50 = Color(0xFFFEEAE6);
+const Color shrinePink100 = Color(0xFFFEDBD0);
+const Color shrinePink300 = Color(0xFFFBB8AC);
+const Color shrinePink400 = Color(0xFFEAA4A4);
+
+const Color shrineBrown900 = Color(0xFF442B2D);
+const Color shrineBrown600 = Color(0xFF7D4F52);
+
+const Color shrineErrorRed = Color(0xFFC5032B);
+
+const Color shrineSurfaceWhite = Color(0xFFFFFBFA);
+const Color shrineBackgroundWhite = Colors.white;
+
+const defaultLetterSpacing = 0.03;
