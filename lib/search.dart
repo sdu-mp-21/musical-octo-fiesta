bc8ffd6f85338c72a802c766555e4940a0925123
190107077 <190107077@stu.sdu.edu.kz>
2021-10-28 12:08:26 +0600

    delete a watermark

--- lib/search.dart
+++ lib/search.dart
@@ -3,18 +3,17 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:rxdart/rxdart.dart';
 
-Map<int, Color> color =
-{
-  50:Color.fromRGBO(51, 153, 255, .1),
-  100:Color.fromRGBO(51, 153, 255, .2),
-  200:Color.fromRGBO(51, 153, 255, .3),
-  300:Color.fromRGBO(51, 153, 255, .4),
-  400:Color.fromRGBO(51, 153, 255, .5),
-  500:Color.fromRGBO(51, 153, 255, .6),
-  600:Color.fromRGBO(51, 153, 255, .7),
-  700:Color.fromRGBO(51, 153, 255, .8),
-  800:Color.fromRGBO(51, 153, 255, .9),
-  900:Color.fromRGBO(51, 153, 255, 1),
+Map<int, Color> color = {
+  50: Color.fromRGBO(51, 153, 255, .1),
+  100: Color.fromRGBO(51, 153, 255, .2),
+  200: Color.fromRGBO(51, 153, 255, .3),
+  300: Color.fromRGBO(51, 153, 255, .4),
+  400: Color.fromRGBO(51, 153, 255, .5),
+  500: Color.fromRGBO(51, 153, 255, .6),
+  600: Color.fromRGBO(51, 153, 255, .7),
+  700: Color.fromRGBO(51, 153, 255, .8),
+  800: Color.fromRGBO(51, 153, 255, .9),
+  900: Color.fromRGBO(51, 153, 255, 1),
 };
 
 class Search extends StatelessWidget {
@@ -23,11 +22,13 @@ class Search extends StatelessWidget {
     return new MaterialApp(
       title: 'Search',
       theme: new ThemeData(scaffoldBackgroundColor: const Color(0xFFFFFF)),
-      home: new MyHomePage(title: 'Search',),
+      home: new MyHomePage(
+        title: 'Search',
+      ),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
-
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -46,20 +47,31 @@ class _MyHomePageState extends State<MyHomePage> {
   bool _isLoading = false;
 
   void _textChanged(String text) {
-    if(text.isEmpty) {
-      setState((){_isLoading = false;});
+    if (text.isEmpty) {
+      setState(() {
+        _isLoading = false;
+      });
       _clearList();
       return;
     }
-    setState((){_isLoading = true;});
+    setState(() {
+      _isLoading = true;
+    });
     _clearList();
-    http.get(Uri.parse('https://www.googleapis.com/books/v1/volumes?q=$text'))
+    http
+        .get(Uri.parse('https://www.googleapis.com/books/v1/volumes?q=$text'))
         .then((response) => response.body)
         .then(jsonDecode)
         .then((map) => map["items"])
-        .then((list) {list.forEach(_addBook);})
+        .then((list) {
+          list.forEach(_addBook);
+        })
         .catchError(_onError)
-        .then((e){setState((){_isLoading = false;});});
+        .then((e) {
+          setState(() {
+            _isLoading = false;
+          });
+        });
   }
 
   void _onError(dynamic d) {
@@ -73,65 +85,67 @@ class _MyHomePageState extends State<MyHomePage> {
       _items.clear();
     });
   }
+
   void _addBook(dynamic book) {
     setState(() {
-      _items.add(new Book(book["volumeInfo"]["title"], book["volumeInfo"]["imageLinks"]["smallThumbnail"]));
+      _items.add(new Book(book["volumeInfo"]["title"],
+          book["volumeInfo"]["imageLinks"]["smallThumbnail"]));
     });
   }
 
   @override
   void initState() {
     super.initState();
-    subject.stream.debounceTime(new Duration(milliseconds: 600)).listen(_textChanged);
+    subject.stream
+        .debounceTime(new Duration(milliseconds: 600))
+        .listen(_textChanged);
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text(widget.title,style: new TextStyle(color: Colors.white)),
+        title:
+            new Text(widget.title, style: new TextStyle(color: Colors.white)),
       ),
       body: new Container(
-
         padding: new EdgeInsets.all(8.0),
         child: new Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
             new TextField(
               cursorColor: Colors.black,
-              style: TextStyle(
-                  color: Colors.black
-              ),
+              style: TextStyle(color: Colors.black),
               decoration: InputDecoration(
                 prefixIcon: Icon(Icons.search),
                 filled: true,
-
-                  hintText: 'Search titles, topics, or authors',
+                hintText: 'Search titles, topics, or authors',
                 fillColor: Colors.grey[100],
                 border: OutlineInputBorder(
                     borderSide: BorderSide(
-                      color : const Color(0xFF000000),
+                      color: const Color(0xFF000000),
                     ),
-                    borderRadius: BorderRadius.circular(8)
-                ),
+                    borderRadius: BorderRadius.circular(8)),
               ),
               onChanged: (string) => (subject.add(string)),
             ),
             SizedBox(height: 10),
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween  ,
-              children:<Widget> [
-
-                Text('Recent',style:TextStyle(
-                    color: Colors.black,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold)),
-                Text('Clear all',style:TextStyle(
-                    color: Colors.grey,
-                    fontSize: 15,)),
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text('Recent',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold)),
+                Text('Clear all',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 15,
+                    )),
               ],
             ),
-            _isLoading? new CircularProgressIndicator(): new Container(),
+            _isLoading ? new CircularProgressIndicator() : new Container(),
             new Expanded(
               child: new ListView.builder(
                 padding: new EdgeInsets.all(8.0),
@@ -142,17 +156,17 @@ class _MyHomePageState extends State<MyHomePage> {
                           padding: new EdgeInsets.all(8.0),
                           child: new Row(
                             children: <Widget>[
-                              _items[index].url != null? new Image.network(_items[index].url): new Container(),
+                              _items[index].url != null
+                                  ? new Image.network(_items[index].url)
+                                  : new Container(),
                               new Flexible(
-                                  child:new Padding(
-                                    padding: new EdgeInsets.all(20.0),
-                                    child: new Text(_items[index].title, maxLines: 9),
-                                  )
-                              ),
+                                  child: new Padding(
+                                padding: new EdgeInsets.all(20.0),
+                                child:
+                                    new Text(_items[index].title, maxLines: 9),
+                              )),
                             ],
-                          )
-                      )
-                  );
+                          )));
                 },
               ),
             ),
