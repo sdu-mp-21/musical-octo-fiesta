a21331bb0add7ad547b59710187dae607df7c1ec
190107077 <190107077@stu.sdu.edu.kz>
2021-10-28 11:16:32 +0600

    add some changes

--- lib/profiledesign.dart
+++ lib/profiledesign.dart
@@ -12,14 +12,40 @@ class _MyProfileDesign extends State<ProfileDesign> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        toolbarHeight: 200,
         title: const Text(
           "@tomholland",
           style: TextStyle(color: Colors.white, fontSize: 20),
         ),
-        leading: const Icon(Icons.account_circle_rounded),
+        centerTitle: true,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(30),
+          ),
+        ),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            color: const Color(0xff7c94b6),
+            image: const DecorationImage(
+              image: AssetImage('assets/images/profilewallpaper.jpg'),
+              fit: BoxFit.cover,
+            ),
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
         elevation: 15,
-        backgroundColor: const Color(0xFF80DEEA),
+        backgroundColor: const Color(0xFF29B6F6),
         actions: const [Icon(Icons.more_vert)],
+      ),
+      body: Container(
+        child: Container(
+          margin: EdgeInsets.only(top: 30),
+          padding: EdgeInsets.all(20),
+          child: CircleAvatar(
+            radius: 80,
+            backgroundImage: AssetImage('assets/images/pctr.jpg'),
+          ),
+        ),
       ),
     );
   }
