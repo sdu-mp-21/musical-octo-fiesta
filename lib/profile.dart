ae5dc72f4eef7b0f29642d751df7673b83b190b5
Altynay Mukhtar <190107054@stu.sdu.edu.kz>
2021-10-28 09:02:31 +0600

    login/password fields

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Profile extends StatefulWidget {
   const Profile({Key key}) : super(key: key);
@@ -7,8 +8,48 @@ class Profile extends StatefulWidget {
 }
 
 class _MyProfile extends State<Profile> {
+  var username;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: Container(
+            color: Colors.cyan[500],
+            child: Column(children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(top: 90),
+                child: Center(
+                  child: Text('Log In',
+                      style: GoogleFonts.pacifico(
+                        textStyle: TextStyle(
+                          color: Colors.white,
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )),
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              TextField(
+                  decoration: InputDecoration(
+                labelText: "Username",
+                hintText: "Enter your email",
+                prefixIcon: Icon(Icons.people),
+              )),
+              SizedBox(
+                height: 60,
+              ),
+              TextField(
+                  decoration: InputDecoration(
+                prefixIcon: Icon(Icons.lock),
+                labelText: "Password",
+                hintText: "Enter you password",
+              )),
+              SizedBox(
+                height: 70,
+              ),
+            ])));
   }
 }
