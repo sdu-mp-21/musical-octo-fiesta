6e834756824e41067b383e02c98e64a54cd02a16
190107077 <190107077@stu.sdu.edu.kz>
2021-11-02 11:26:17 +0600

    changed

--- lib/profiledesign.dart
+++ lib/profiledesign.dart
@@ -11,7 +11,7 @@ class _MyProfileDesign extends State<ProfileDesign> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF21BFBD),
+      backgroundColor: Colors.blue,
       body: ListView(
         children: <Widget>[
           Row(
@@ -21,14 +21,46 @@ class _MyProfileDesign extends State<ProfileDesign> {
                   onPressed: () {},
                   icon: Icon(Icons.arrow_back_ios_new_sharp),
                   color: Colors.white,
+                  padding: EdgeInsets.all(10),
                 ),
                 IconButton(
                   onPressed: () {},
-                  icon: Icon(Icons.menu),
+                  icon: Icon(Icons.border_color_sharp),
                   //padding: EdgeInsets.all(10),
                   color: Colors.white,
+                  padding: EdgeInsets.all(10),
                 ),
               ]),
+          SizedBox(height: 25.0),
+          Padding(
+            padding: EdgeInsets.only(left: 40.0),
+            child: Row(
+              children: <Widget>[
+                Column(children: <Widget>[
+                  Text('Tom Holland',
+                      style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25.0)),
+                  Text('tomholland@gmail.com',
+                      style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontWeight: FontWeight.w100,
+                        fontSize: 18,
+                        color: Colors.white,
+                      )),
+                ]),
+                Padding(
+                  padding: EdgeInsets.only(left: 15),
+                  child: CircleAvatar(
+                    radius: 60,
+                    backgroundImage: AssetImage('assets/images/pctr.jpg'),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
