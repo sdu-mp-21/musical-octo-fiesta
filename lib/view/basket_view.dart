39cc3393e2e70d8dcfa1d31e5174146413a7f648
190107077 <190107077@stu.sdu.edu.kz>
2021-12-02 10:46:20 +0600

    changed

--- lib/view/basket_view.dart
+++ lib/view/basket_view.dart
@@ -1,12 +1,55 @@
 import "package:flutter/material.dart";
+import 'package:BookStore/models/book.dart';
+import 'package:BookStore/services/books.dart';
+import 'package:BookStore/view/widgets/book_card_in_basket_widget.dart';
+import 'package:flutter/widgets.dart';
 
-class Basket extends StatelessWidget {
+class Basket extends StatefulWidget {
+  const Basket({Key key, this.id}) : super(key: key);
+
+  final String id;
+  @override
+  _MyBasket createState() => new _MyBasket();
+}
+
+class _MyBasket extends State<Basket> {
+  List<Widget> bookList(List<Book> books) {
+    List<Widget> list = [];
+    for (Book book in books) {
+      list.add(new BookCard(book));
+    }
+    return list;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container()
-    );
+        body: Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/baner2.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: FutureBuilder(
+          future: getAllBooks(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.waiting:
+                return Text("Loading..");
+              default:
+                if (snapshot.hasError) {
+                  return Text('Error');
+                } else {
+                  return GridView.count(
+                      // Create a grid with 2 columns. If you change the scrollDirection to
+                      // horizontal, this produces 2 rows.
+                      crossAxisCount: 2,
+                      // Generate 100 widgets that display their index in the List.
+                      children: bookList(snapshot.data));
+                }
+            }
+          }),
+    ));
   }
 }
-
-
---
+++ lib/view/widgets/book_card_in_basket_widget.dart
@@ -0,0 +1,119 @@
+import 'package:BookStore/models/book.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../item_details_view.dart';
+
+class BookCard extends StatelessWidget {
+  final Book book;
+
+  BookCard(this.book);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => MyItemPage(id: book.id)),
+          );
+        },
+        child: Padding(
+            padding: EdgeInsets.all(10),
+            child: Stack(children: [
+              SizedBox(
+                  width: 120,
+                  height: 230,
+                  child: Column(children: [
+                    DecoratedBox(
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: CachedNetworkImage(
+                            height: 180,
+                            width: 150,
+                            fit: BoxFit.cover,
+                            imageUrl: book.image,
+                            placeholder: (context, url) => Icon(Icons.image),
+                            errorWidget: (context, url, error) =>
+                                Icon(Icons.error),
+                          ),
+                        ),
+                        decoration: BoxDecoration(
+                          color: Colors.transparent,
+                          borderRadius: BorderRadius.circular(20),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Color.fromRGBO(0, 0, 0, 0.05),
+                              blurRadius: 5,
+                              spreadRadius: 1,
+                              offset: Offset(0, 6), // Shadow position
+                            ),
+                          ],
+                        )),
+                    SizedBox(
+                      height: 124,
+                    ),
+                    Text(
+                      book.title,
+                      style: TextStyle(fontWeight: FontWeight.w800),
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    Text(
+                      book.price.toString() + " ₸",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 14,
+                          color: Colors.black38),
+                    ),
+                  ])),
+              Positioned(
+                  top: 10,
+                  left: 0,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 5, vertical: 2),
+                    decoration: BoxDecoration(
+                      color: Colors.green.shade600,
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(10),
+                          bottomRight: Radius.circular(10)),
+                    ),
+                    child: Row(
+                      children: [
+                        Text(
+                          book.rating.toString(),
+                          style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 12,
+                              color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  )),
+              Positioned(
+                  bottom: 10,
+                  left: 0,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 7, vertical: 2),
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(50),
+                        bottomRight: Radius.circular(50),
+                        topLeft: Radius.circular(50),
+                        bottomLeft: Radius.circular(50),
+                      ),
+                    ),
+                    child: Row(
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.delete_outlined),
+                          color: Colors.black,
+                          onPressed: () {},
+                          iconSize: 20,
+                        ),
+                      ],
+                    ),
+                  )),
+            ])));
+  }
+}
